a1bb47a29b181a3fb39361143cd70a6d6c4f56ab
achrafedd <achrafeddiba@gmail.com>
2023-10-03 17:46:45 +0100

    task 101: remove char at

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,10 @@
 def remove_char_at(str, n):
     if len(str) < n and n < 0:
         return str
-    str[n] = ""
-    return str
+    chars = range(0, len(str))
+    new_str = ""
+    for i in chars:
+        if i == n:
+            continue
+        new_str += str[i]
+    return new_str
