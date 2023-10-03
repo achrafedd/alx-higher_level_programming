2761e1ad93e0b017aa1062606ec1787cbba592a0
achrafedd <achrafeddiba@gmail.com>
2023-10-03 15:18:50 +0100

    task 8: uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        is_lower = ord(c) >= 97 and ord(c) <= 122
+        print("{:c}".format(ord(c) - 32 if is_lower else ord(c)), end="")
+    print("")
+
+
+uppercase("best")
