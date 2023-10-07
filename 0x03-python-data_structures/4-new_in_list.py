bebe5a3451c2cbbda53e405c4572c656b9616844
achrafedd <achrafeddiba@gmail.com>
2023-10-07 14:51:48 +0100

    task 4: new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(new_list):
+        return None
+    new_list[idx:idx+1] = [element]
+    return new_list
