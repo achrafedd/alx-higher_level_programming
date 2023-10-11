8a812b9def97be8e1c756520b585131d529848c7
achrafedd <achrafeddiba@gmail.com>
2023-10-11 15:23:38 +0100

    task 0: square matrix simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for l in matrix:
+        arr = []
+        for num in l:
+            arr.append(num ** 2)
+        new_list.append(arr)
+    return new_list
