217695515ee628075f8d64c8c7cfd077f4f5cb66
achrafedd <achrafeddiba@gmail.com>
2023-10-23 17:47:09 +0100

    fix task 0

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,9 @@
 import sys
 
 def safe_function(fct, *args):
-    result = None
     try:
         result = fct(args[0], args[1])
     except Exception as err:
-        result = None
         sys.stderr.write("Exception: {}\n".format(err.args[0]))
+        return None
     return result
