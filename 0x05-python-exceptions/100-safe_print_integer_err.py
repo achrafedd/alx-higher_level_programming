98a312b3cf56fa70ad6ac658d75725ccc38878bf
achrafedd <achrafeddiba@gmail.com>
2023-10-23 17:19:15 +0100

    fix task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,9 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as err:
-        sys.stderr.write("Exception: {}".format(err.args[0]))
+        sys.stderr.write("Exception: {}\n".format(err.args[0]))
         return False
     except TypeError as err:
-        sys.stderr.write("Exception: {}".format(err.args[0]))
+        sys.stderr.write("Exception: {}\n".format(err.args[0]))
         return False
     return True
