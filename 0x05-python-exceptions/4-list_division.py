a25013eabd83e0861a0b34963dde73cde88f71da
achrafedd <achrafeddiba@gmail.com>
2023-10-23 15:44:55 +0100

    task 4: list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    new_list = []
+    while i < list_length:
+        try:
+            if type(my_list_1[i]) not in [int, float] or type(my_list_2[i]) not in [int, float]:
+                raise TypeError
+            new_list.append(my_list_1[i] / my_list_2[i])
+            i += 1
+        except TypeError:
+            i += 1
+            new_list.append(0)
+            print("wrong type")
+        except ZeroDivisionError:
+            i += 1
+            new_list.append(0)
+            print("division by 0")
+        except IndexError:
+            i += 1
+            new_list.append(0)
+            print("out of range")
+    return new_list
