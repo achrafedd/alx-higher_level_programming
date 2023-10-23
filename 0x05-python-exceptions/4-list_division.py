00b6be94105b8d73a6ca5ecc42ab0c8b5c73110d
achrafedd <achrafeddiba@gmail.com>
2023-10-23 15:46:33 +0100

    task 4: list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -21,4 +21,6 @@ def list_division(my_list_1, my_list_2, list_length):
             i += 1
             new_list.append(0)
             print("out of range")
+        finally:
+            pass
     return new_list
