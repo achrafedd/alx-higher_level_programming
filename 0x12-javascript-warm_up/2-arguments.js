3b28353365cc9018a376ff7821bf23b86e3dca94
achrafedd <achrafeddiba@gmail.com>
2024-01-08 21:16:30 +0100

    task 2: arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require("node:process");
+
+const length = argv.length;
+
+if (length <= 2) {
+	console.log('No argument');
+} else if (length == 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
