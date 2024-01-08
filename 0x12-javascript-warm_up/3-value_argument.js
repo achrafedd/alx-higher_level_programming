c1ba2a50c49c293eedbc42ef2aff0f36862d07d3
achrafedd <achrafeddiba@gmail.com>
2024-01-08 21:36:14 +0100

    fix task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const length = argv.length;
 
-if (length <= 2) {
-  console.log('No argument');
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log('No argument');
 }
