ffeeb92c196a9d267d0edf73fc8a9814a2f90f23
achrafedd <achrafeddiba@gmail.com>
2024-01-08 23:14:02 +0100

    task 11: second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let [_0, _1, ...numbers] = process.argv;
+const length = numbers.length;
+const output =
+  length > 1
+    ? numbers.map((number) => +number).sort((a, b) => a - b)[length - 2]
+    : 0;
+console.log(output)
