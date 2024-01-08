14234faa83c5ec0724dc01060e1c6274635bbf3a
achrafedd <achrafeddiba@gmail.com>
2024-01-08 22:55:24 +0100

    task 10:factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 
-const factorial = (number) => {
+function factorial (number) {
   if (number === 1 || isNaN(number)) {
     return 1;
   }
   return number * factorial(number - 1);
-};
+}
 
 console.log(factorial(number));
