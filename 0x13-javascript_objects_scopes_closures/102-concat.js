1b871dd46419528c9600eb059e01760e31515508
achrafedd <achrafeddiba@gmail.com>
2024-01-10 00:10:01 +0100

    task 102: concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const f1 = fs.readFileSync(process.argv[2], 'utf-8');
+const f2 = fs.readFileSync(process.argv[3], 'utf-8');
+fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');
