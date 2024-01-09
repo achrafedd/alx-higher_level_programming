582d7e34db6628ec67f33fd24cc304777a682634
achrafedd <achrafeddiba@gmail.com>
2024-01-09 21:52:32 +0100

    fix task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w >= 0 && h >= 0) return;
+    if (w <= 0 || h <= 0) return;
+    if (typeof w !== 'number' || typeof h !== 'number') return;
     this.width = w;
     this.height = h;
   }
