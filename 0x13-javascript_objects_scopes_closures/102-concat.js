e8459dbb64fcfceb30a7c62aae3bc19f87ddde76
achrafedd <achrafeddiba@gmail.com>
2024-01-10 00:13:54 +0100

    fix task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 
 const f1 = fs.readFileSync(process.argv[2], 'utf-8');
 const f2 = fs.readFileSync(process.argv[3], 'utf-8');
-fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');
+fs.writeFileSync(process.argv[4], f1 + '\n' + f2, 'utf-8');
