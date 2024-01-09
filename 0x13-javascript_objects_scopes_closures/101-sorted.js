78ca738d67e9cbc942de8fc4ec7c15fb69836abb
achrafedd <achrafeddiba@gmail.com>
2024-01-09 23:58:32 +0100

    task 101: sorted

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { dict } = require('101-data.js');
+const newDict = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] === undefined) newDict[value] = [];
+  newDict[value] = [...newDict[value], key];
+}
+
+console.log(newDict);
