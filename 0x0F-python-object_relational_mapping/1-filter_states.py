c2d0969010e8f380d5ee8e01c2d1d2b5d2c55d2c
achrafedd <achrafeddiba@gmail.com>
2024-01-14 16:02:59 +0100

    task 1: filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,10 +6,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""
-                SELECT * FROM states
-                WHERE name LIKE 'N%'
-                ORDERD BY states.id""")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
