a84c7fd69dcb70d4cb73c87a1344dc528f54ee3b
achrafedd <achrafeddiba@gmail.com>
2024-01-14 17:55:20 +0100

    task 5: filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" select all cities """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT c.name FROM cities AS c
+                INNER JOIN states AS s
+                WHERE c.state_id = s.id
+                AND s.name = %s
+                ORDER BY c.id""", (argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        if row == rows[-1]:
+           print(row[0])
+        else:
+          print(row[0], end=', ')
+    
+    cur.close()
+    db.close()
