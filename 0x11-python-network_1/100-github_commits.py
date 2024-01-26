337aa08fb29bd64d6b6d5836f7b593041c05805d
achrafedd <achrafeddiba@gmail.com>
2024-01-26 23:15:54 +0100

    task 100: github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    res = requests.get(url)
+    for i in range(10):
+        print(f"{res.json()[i]['sha']}: {res.json()[i]['commit']['author']['name']}")
