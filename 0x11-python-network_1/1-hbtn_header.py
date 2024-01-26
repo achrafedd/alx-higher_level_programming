4662101bc9cb5856ba84ffa173691dc15c582eb8
achrafedd <achrafeddiba@gmail.com>
2024-01-26 21:47:38 +0100

    fix: fix request for task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """Display the value of X-Request-Id from header"""
-
-from urllib import request
 import sys
-
-url = sys.argv[1]
-
-req = request.Request(url)
-with request.urlopen(req) as res:
-    res_id = res.headers["X-Request-Id"]
+import urllib.request
 
 if __name__ == "__main__":
-    print(res_id)
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))
