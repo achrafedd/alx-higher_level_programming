b623a006fcbfa25afed0106c2e09263f914c3d67
achrafedd <achrafeddiba@gmail.com>
2024-01-26 23:18:45 +0100

    task 100: github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,5 +9,8 @@ if __name__ == "__main__":
 
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     res = requests.get(url)
-    for i in range(10):
-        print(f"{res.json()[i]['sha']}: {res.json()[i]['commit']['author']['name']}")
+    try:
+        for i in range(10):
+            print(f"{res.json()[i]['sha']}: {res.json()[i]['commit']['author']['name']}")
+    except IndexError:
+        pass
